75c4e4e62a0a45c9f62fe3ce7ef1fb0d4edb15c5
shihuang <gkshi@foxmail.com>
2021-08-07 17:06:12 +0800

    chore: 优化构建过程

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,30 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'lib/index.ts'),
-      name: 'v3-waterfall'
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
-        }
+const target = process.env.TARGET
+let buildConfig = {
+  lib: {
+    entry: path.resolve(__dirname, 'lib/index.ts'),
+    name: 'v3-waterfall'
+  },
+  rollupOptions: {
+    external: ['vue'],
+    output: {
+      globals: {
+        vue: 'Vue'
       }
     }
-  },
+  }
+}
+
+if (target === 'page') {
+  buildConfig = {
+    outDir: 'github-page'
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: { ...buildConfig },
   plugins: [vue()]
 })
